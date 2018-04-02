8e3a1f20743fa8e6419be027f194021364aa04bb
Teddi Maull <teddimaull@gmail.com>
2018-04-02 15:48:24 -0600

    add signup button to splash page

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faSync from '@fortawesome/fontawesome-free-solid/faSync'
 
+import RaisedButton from 'material-ui/RaisedButton';
+
 class SplashPage extends Component {
   render() {
     return (
@@ -10,6 +12,7 @@ class SplashPage extends Component {
           <FontAwesomeIcon icon={faSync} className='fa-10x' />
           <h1> Relationship App </h1>
           <p> An app designed to help you reconnect with your partner. </p>
+          <RaisedButton label="Sign Up" default={true} />
         </div>
       </div>
     )
