30edcd4c5e18c95d221ca2fb1b900bacfd32abe7
Teddi Maull <teddimaull@gmail.com>
2018-04-03 15:49:53 -0600

    Make signup text consistent with wireframes

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -7,12 +7,12 @@ import RaisedButton from 'material-ui/RaisedButton';
 class SplashPage extends Component {
   render() {
     return (
-      <div className="bg">
+      <div className="splash bg">
         <div className="inner">
           <FontAwesomeIcon icon={faSync} className='fa-10x' />
           <h1> Relationship App </h1>
           <p> An app designed to help you reconnect with your partner. </p>
-          {/* <RaisedButton label="Sign Up" default={true} /> */}
+          <RaisedButton label="Create Account" default={true} />
         </div>
       </div>
     )
