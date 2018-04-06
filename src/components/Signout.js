15b2d9aa69b81b709d09d4c9fc506081d9dc1551
Teddi Maull <teddimaull@gmail.com>
2018-04-06 14:11:35 -0600

    add sign out functionality

---
+++ src/components/Signout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { auth } from '../firebase'
+
+const SignOutButton = () =>
+  <button
+    type="button"
+    onClick={auth.signOut}
+  >
+    Sign Out
+  </button>
+
+export default SignOutButton
