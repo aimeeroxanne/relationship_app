dfce1562cfdcc86886087966a99854fddf1df96b
Teddi Maull <teddimaull@gmail.com>
2018-04-06 14:10:32 -0600

    add routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,8 +1,17 @@
 
-const SIGN_UP = "signup"
-const HOME = "app"
+
+const SIGN_UP = '/signup'
+const SIGN_IN = '/signin'
+const SPLASH = '/'
+const HOME = '/'
+const ACCOUNT = '/account'
+const PASSWORD_FORGET = '/pw-forget'
 
 export {
   SIGN_UP,
-  HOME
+  SIGN_IN,
+  SPLASH,
+  HOME,
+  ACCOUNT,
+  PASSWORD_FORGET
 }
