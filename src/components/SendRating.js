87620ac80ed0bce63adf00147f743f877d1f1202
Teddi Maull <teddimaull@gmail.com>
2018-05-02 22:10:18 -0600

    add skeleton components

---
+++ src/components/SendRating.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faSync from '@fortawesome/fontawesome-free-solid/faSync'
+import { Link, Switch } from "react-router-dom";
+
+import RaisedButton from 'material-ui/RaisedButton';
+
+class SendRating extends Component {
+  render() {
+    return (
+      <div className="splash bg">
+        <div className="inner">
+          <FontAwesomeIcon icon={faSync} className='fa-10x' />
+          <h1> Relationship App </h1>
+          <p> An app designed to help you reconnect with your partner. </p>
+          <Link to="/signup" ><RaisedButton label="Create Account" default={true} /></Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SendRating
